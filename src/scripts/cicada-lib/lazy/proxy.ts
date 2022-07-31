98a25cd3fe26fb9ab6885514228b4ed740d2e2bf
PHO <pho@cielonegro.org>
2022-07-31 21:15:55 +0900

    Improve the lazy proxy

--- src/scripts/cicada-lib/lazy/proxy.ts
+++ src/scripts/cicada-lib/lazy/proxy.ts
@@ -7,8 +7,30 @@ export function lazy<T>(thunk: () => T): T {
     let value: T;
 
     return new Proxy({}, {
-        get(_target: any, key: PropertyKey, _receiver: any): any {
-            if (!isEvaluated) value = thunk();
+        apply(_targ: any, _this: any, args: any[]): any {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return (value as any).call(value, ...args);
+        },
+        construct(_targ: any, args: any[]): any {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return new (value as any)(...args);
+        },
+        defineProperty(_targ: any, key: PropertyKey, desc: PropertyDescriptor): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            Object.defineProperty(value, key, desc);
+            return true;
+        },
+        deleteProperty(_targ: any, key: PropertyKey): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            delete (value as any)[key];
+            return true;
+        },
+        get(_targ: any, key: PropertyKey): any {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
 
             const prop: any = (value as any)[key];
             if (typeof prop === "function") {
@@ -20,10 +42,47 @@ export function lazy<T>(thunk: () => T): T {
                 return prop;
             }
         },
-        set(_target: any, key: PropertyKey, v: any, _receiver: any): boolean {
-            if (!isEvaluated) value = thunk();
+        getOwnPropertyDescriptor(_targ: any, key: PropertyKey): PropertyDescriptor|undefined {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return Object.getOwnPropertyDescriptor(value, key);
+        },
+        getPrototypeOf(_targ: any): any|null {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return Object.getPrototypeOf(value);
+        },
+        has(_targ: any, key: PropertyKey): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return key in value;
+        },
+        isExtensible(_targ: any): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return Object.isExtensible(value);
+        },
+        ownKeys(_targ: any): (string|symbol)[] {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            return Reflect.ownKeys(value as any);
+        },
+        preventExtensions(_targ: any): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            Object.preventExtensions(value);
+            return true;
+        },
+        set(_targ: any, key: PropertyKey, v: any): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
 
             (value as any)[key] = v;
+            return true;
+        },
+        setPrototypeOf(_targ: any, proto: any|null): boolean {
+            if (!isEvaluated) { value = thunk(); isEvaluated = true }
+
+            Object.setPrototypeOf(value, proto);
             return true;
         }
     });
